f953212d90974d12d36c8cdc9c144368e033fd2d
Onabanjo Oluwatobiloba <onabanjt@gmail.com>
2023-06-14 22:02:22 +0100

    First Commit

---
+++ Genin/Day 1.py
@@ -0,0 +1,10 @@
+# Band name generator
+
+print("Welcome to the Band Name genrator")
+
+band_name = input("What City Did You Grow Up In?:\n")
+
+pet_name = input("What is the name of your pet animal?:\n")
+
+print(f"Your band name could be {band_name} {pet_name}")
+
